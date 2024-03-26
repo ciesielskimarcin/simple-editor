c8e43b1511bb05ca2dcb5683fe722231ae6baaab
Marcin Ciesielski <marcin.ciesielski@edrmedeso.com>
2024-03-26 11:38:06 +0100

    added Head & OpenGraph and animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,18 @@ module.exports = {
     "./src/**/*.{html,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        wiggle: 'wiggle 1s ease-in-out infinite',
+      },
+      
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-3deg)' },
+          '50%': { transform: 'rotate(3deg)' },
+        }
+      }
+    },
   },
   plugins: [],
 }
